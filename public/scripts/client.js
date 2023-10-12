f9e9e7003355152f8e1853e9d1701a8400f2f5d2
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-10-12 18:12:01 -0400

    add tweet submission logic, and refresh with new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,35 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 
 const createTweetElement = function(tweetObj) {
@@ -46,7 +46,7 @@ const createTweetElement = function(tweetObj) {
         </article>
         <hr class="default">
         <footer class="header-tweets">
-          <div>${tweetObj.created_at}</div>
+          <div>${timeago.format(tweetObj.created_at)}</div>
           <div>
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -72,20 +72,49 @@ const renderTweets = function(tweets) {
 };
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// };
 
 // const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
 // console.log($tweet); // to see what it looks like
-renderTweets(data);
+// renderTweets(data);
+
+const loadTweets = () => {
+  $.get('/tweets', (data) => {
+    renderTweets(data);
+  });
+};
+loadTweets();
+
+$('form').on('submit', function(event) {
+  event.preventDefault();
+  const data = $('form').serialize();
+  const counter = $(this).closest('.new-tweet').find('.counter-val').val();
+  if (counter >= 140 || counter < 0) {
+    console.log(counter);
+    alert('the tweet is not between 1 - 140 characters');
+  } else {
+    console.log(counter);
+    $('form').trigger('reset');
+
+    //update the character counter value
+    $(this).closest('.new-tweet').find('.counter-val').text(140);
+    $.post('/tweets', data);
+    $('#tweets-container').empty()
+    loadTweets()
+  }
+
+
+});
+
